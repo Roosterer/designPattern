9ad9c5b9709f9f32d2dc2ccf3a760cfc2c84fdac
wb-gaokuo <goakuo_ok@163.com>
2018-03-05 17:02:54 +0800

    增加问题

--- src/main/java/com/gk/类加载器/TestUrlClassLoader.java
+++ src/main/java/com/gk/类加载器/TestUrlClassLoader.java
@@ -19,7 +19,21 @@ public class TestUrlClassLoader {
         URL[] urs = new URL[]{url};
         URLClassLoader urlClassLoader = new URLClassLoader(urs);
         Object c = urlClassLoader.loadClass("com.gk.StringUtils");
-        System.out.println(c);
+        System.out.println(c.hashCode());
+
+        URLClassLoader urlClassLoader2 = new URLClassLoader(urs);
+        Object c2 = urlClassLoader2.loadClass("com.gk.StringUtils");
+        System.out.println(c2.hashCode());
+        //c c2 有相同的hashcode，所以是肯定是同一个类加载器加载的
+
+        System.out.println("===================");
+        System.out.println(urlClassLoader.hashCode());
+        System.out.println(urlClassLoader2.hashCode());
+
+        //之所以类加载器不同，但是加载同一个文件能得到相同的class，是因为，urlClassLoader的loadClass调用的
+        //是父类ClassLoad的loadClass方法，先判断缓存，然后找父类加载器加载。理论是来说这是两个不同的类加载器
+        //如果缓存是保存再每个实例中，显然，这样加载会得到不同的class，所以我判断这是一个公共缓存。fuck，不清楚
+        //TODO ? 问题
     }
 
 }
